5b74dd8cc168ce2870380887c267bfa39813b9b5
Vyraal1 <kevin.afable@gmail.com>
2020-02-17 10:51:25 -0500

    feat: insertion sort - nested for/while loop

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,7 +1,26 @@
-def selection_sort(arr):
-    """Selection sort is an sort that starts by looking for the smallest value in a list, and making that the first element in the list. Then it'll use a primary iterative loop through the array to keep track of which index (and thus next smallest number). Then it will also use a secondary loop to check for the smallest number again in the rest of the list. Once it finds the smallest number, it'll swap to its correct place (the current index) and then restart the process again.
+def insertion_sort(arr):
+    """Insertion sort is a sort that starts as a smaller sub-list of the target list, starting at one element. It takes in the target list one at a time, inputting the new incoming element into its correct index before moving on with the sort."""
 
-    It's time complexity is O(n squared).
+    for i in range(1, len(arr)):
+        cur = i
+        j = i-1
+        # while cur value is higher than everything below it
+        while arr[cur] < arr[j] and j >= 0:
+            # swap
+            temp = arr[cur]
+            arr[cur] = arr[j]
+            arr[j] = temp
+            # keep track of the element as it moves down
+            cur = j
+            j = j - 1
+
+    return arr
+
+
+def selection_sort(arr):
+    """Selection sort is a sort that starts by looking for the smallest value in a list, and making that the first element in the list. Then it'll use a primary iterative loop through the array to keep track of which index (and thus next smallest number). Then it will also use a secondary loop to check for the smallest number again in the rest of the list. Once it finds the smallest number, it'll swap to its correct place (the current index) and then restart the process again.
+
+    It's time complexity is at worst is O(n squared).
     """
     for i in range(0, len(arr) - 1):
         cur_index = i
