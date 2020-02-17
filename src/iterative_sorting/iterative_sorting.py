3bcacec9aba7d537e3b5a383f0e470c1531eb89c
Vyraal1 <kevin.afable@gmail.com>
2020-02-16 23:27:07 -0500

    feat: naive selection doc

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,5 +1,8 @@
 def selection_sort(arr):
-    """Selection sort is an sort that starts by looking for the smallest value in a list, and making that the first element in the list."""
+    """Selection sort is an sort that starts by looking for the smallest value in a list, and making that the first element in the list. Then it'll use a primary iterative loop through the array to keep track of which index (and thus next smallest number). Then it will also use a secondary loop to check for the smallest number again in the rest of the list. Once it finds the smallest number, it'll swap to its correct place (the current index) and then restart the process again.
+
+    It's time complexity is O(n squared).
+    """
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
